3dcc6b164a3b07313ffe07c4e0dd171ccc8945a7
vaxpleer <miron.sam25@yandex.ru>
2026-07-29 14:51:02 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,7 +8,8 @@ const swiper = new Swiper('.swiper', {
   pagination: {
     el: '.projects-pagination',
     type: 'bullets',
-    bulletClass: '.projects-bullet',
-    bulletActiveClass: '.projects-bullet-active',
+    bulletClass: 'projects-bullet',
+    bulletActiveClass: 'projects-bullet-active',
+    clickable: true,
   },
 });
