bba05e4d996804fe07c8e118d277c63c16b0a8a7
vaxpleer <miron.sam25@yandex.ru>
2026-07-29 14:39:25 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 new WOW({
   animateClass: 'animate__animated'
 }).init();
+
+const swiper = new Swiper('.swiper', {
+  speed: 400,
+  spaceBetween: 100,
+  pagination: {
+    el: '.projects-pagination',
+    type: 'bullets',
+    bulletClass: '.projects-bullet',
+    bulletActiveClass: '.projects-bullet-active',
+  },
+});
